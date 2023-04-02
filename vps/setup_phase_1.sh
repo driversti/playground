162a90c0b63fc614fc3ed2c56ac2faeb8e65105e
Yurii Chekhotskyi <github@seniorjava.dev>
2023-04-02 16:57:43 +0200

    Update setup_phase_1.sh

--- vps/setup_phase_1.sh
+++ vps/setup_phase_1.sh
@@ -1,24 +1,47 @@
-set -e
+#!/bin/bash
 
-sudo apt update
-#sudo apt install openssh-server nano git wget -y
-sudo apt install nano git wget curl zip unzip -y
-echo "Existing updates:"
-sudo apt list --upgradable
-printf "Would you like to upgrade the system? [y/n]: "
-read -r upgrade_resp
-if [ "$upgrade_resp" = "y" ]; then
-    sudo apt upgrade -yy
-else
-  echo "Upgrading skipped"
-fi
-echo "Installing sdkman and the latest LTS java"
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
-sdk install java
-sudo apt install zsh -y
-zsh --version
-sudo chsh -s "$(which zsh)"
-echo "Installing oh-my-zsh"
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+apt update
+apt install nano git wget curl zip unzip sudo -y
+
+# Create a new user called 'testuser'
+adduser --gecos "" testuser
+
+# Grant 'testuser' sudo privileges
+usermod -aG sudo testuser
+
+# Set the password for the testuser
+echo "Enter a password for the new user 'testuser':"
+passwd testuser
+
+# Print success message
+echo "User 'testuser' created successfully with sudo privileges."
+
+# Switch to the new user
+su - testuser << 'EOF'
+# Install Zsh
+su -c 'apt install zsh -y'
+su -c 'chsh -s "$(which zsh)" $USER'
+
+# Install Oh-My-Zsh for the new user
+echo "Installing Oh-My-Zsh for testuser"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+
+# Update Oh-My-Zsh settings
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="steeef"/g' ~/.zshrc
+
+# Install plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# Enable plugins
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions history-substring-search)/g' ~/.zshrc
+
+# Load the new configuration
+source ~/.zshrc
+
+# Exit the subshell
+EOF
+
+# Switch to the testuser with zsh as a shell
+su - testuser -s /bin/zsh
